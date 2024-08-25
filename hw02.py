b17e6762793d7b51275002158f0c320b300d4bc1
AymaanSaheer <aymaan476@gmail.com>
2024-08-24 18:19:23 -0700

    update1

--- hw02.py
+++ hw02.py
@@ -3,5 +3,5 @@ empAge = 40
 yearlySalary = 100000
 fullTimeEmployee = True
 outcome = (f"Employee Name : {empName.upper()}, Age : {empAge}, Yearly Salary : ${yearlySalary:,.2f} and Full Time Status: {fullTimeEmployee}.")
-print (outcome)
+print (outcome) #
             
